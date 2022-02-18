231f71def6acfaf2eec05661a77bfe50ffccd211
psuedodn <99954396+psuedodn@users.noreply.github.com>
2022-02-18 15:12:59 +0530

    pseudo #2

---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,222 @@
+import React from 'react';
+const Banner = () => {
+  return (
+    <div>
+      <div id="home" className="rn-slider-area">
+        <div className="slide slider-style-4 bg_image bg_image--1">
+          <div className="container">
+            <div className="row">
+              <div className="circleMas_align">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 870 870">
+                  <g
+                    id="Group_40954"
+                    data-name="Group 40954"
+                    transform="translate(-1842 -109)"
+                  >
+                    <g id="Group_40952" data-name="Group 40952">
+                      <circle
+                        id="Ellipse_31290"
+                        data-name="Ellipse 31290"
+                        cx="435"
+                        cy="435"
+                        r="435"
+                        transform="translate(1842 109)"
+                        fill="rgba(255,243,178,0.28)"
+                      />
+                      <g id="c2">
+                        <circle
+                          id="Ellipse_31291"
+                          data-name="Ellipse 31291"
+                          cx="352"
+                          cy="352"
+                          r="352"
+                          transform="translate(1925 192)"
+                          fill="rgba(255,238,133,0.6)"
+                        />
+                        <path
+                          id="Path_40575"
+                          data-name="Path 40575"
+                          d="M-354-12A12.006,12.006,0,0,1-342,0a12.006,12.006,0,0,1-12,12A12.006,12.006,0,0,1-366,0,12.006,12.006,0,0,1-354-12Z"
+                          transform="translate(2311.05 535) rotate(64.382)"
+                          fill="#ff1600"
+                        />
+                      </g>
+                      <g id="c3">
+                        <circle
+                          id="Ellipse_31292"
+                          data-name="Ellipse 31292"
+                          cx="258"
+                          cy="258"
+                          r="258"
+                          transform="translate(2019 286)"
+                          fill="rgba(254,232,95,0.45)"
+                        />
+                        <path
+                          id="Path_40576"
+                          data-name="Path 40576"
+                          d="M-195-6a6,6,0,0,1,6,6,6,6,0,0,1-6,6,6,6,0,0,1-6-6A6,6,0,0,1-195-6Z"
+                          transform="translate(2518 636) rotate(87.913)"
+                          fill="#ff1600"
+                        />
+                      </g>
+                      <g id="c4">
+                        <circle
+                          id="Ellipse_31293"
+                          data-name="Ellipse 31293"
+                          cx="188"
+                          cy="188"
+                          r="188"
+                          transform="translate(2089 356)"
+                          fill="rgba(255,213,58,0.4)"
+                        />
+                        <path
+                          id="Path_40577"
+                          data-name="Path 40577"
+                          d="M-238.42-3a7.583,7.583,0,0,1,7.58,7.58,7.583,7.583,0,0,1-7.58,7.58A7.583,7.583,0,0,1-246,4.58,7.583,7.583,0,0,1-238.42-3Z"
+                          transform="matrix(-0.986, 0.168, -0.168, -0.986, 1863.467, 542.494)"
+                          fill="#ff1600"
+                        />
+                      </g>
+                      <g id="c1">
+                        <circle
+                          id="Ellipse_31294"
+                          data-name="Ellipse 31294"
+                          cx="142"
+                          cy="142"
+                          r="142"
+                          transform="translate(2135 402)"
+                          fill="rgba(255,163,58,0.51)"
+                        />
+                        <g
+                          id="c1-2"
+                          data-name="c1"
+                          transform="translate(2229 534) rotate(-71.927)"
+                        >
+                          <path
+                            id="Path_40565"
+                            data-name="Path 40565"
+                            d="M-126-18.5A18.509,18.509,0,0,1-107.5,0,18.509,18.509,0,0,1-126,18.5,18.509,18.509,0,0,1-144.5,0,18.509,18.509,0,0,1-126-18.5Z"
+                            fill="rgba(0,0,0,0)"
+                            stroke="#00fde2"
+                            strokeWidth="1"
+                          />
+                          <path
+                            id="Path_40566"
+                            data-name="Path 40566"
+                            d="M-126-18.5A18.509,18.509,0,0,1-107.5,0,18.509,18.509,0,0,1-126,18.5,18.509,18.509,0,0,1-144.5,0,18.509,18.509,0,0,1-126-18.5Z"
+                            fill="#ff1600"
+                          />
+                        </g>
+                      </g>
+                      <g
+                        id="Group_40951"
+                        data-name="Group 40951"
+                        transform="translate(2277 544)"
+                      >
+                        <path
+                          id="Path_40563"
+                          data-name="Path 40563"
+                          d="M0-103A103.051,103.051,0,0,1,103,0,103.051,103.051,0,0,1,0,103,103.051,103.051,0,0,1-103,0,103.051,103.051,0,0,1,0-103Z"
+                          fill="rgba(0,0,0,0)"
+                          stroke="#00fde2"
+                          strokeWidth="1"
+                        />
+                        <path
+                          id="Path_40564"
+                          data-name="Path 40564"
+                          d="M0-103A103.051,103.051,0,0,1,103,0,103.051,103.051,0,0,1,0,103,103.051,103.051,0,0,1-103,0,103.051,103.051,0,0,1,0-103Z"
+                        />
+                      </g>
+                    </g>
+                    <g
+                      id="Group_40953"
+                      data-name="Group 40953"
+                      transform="translate(474.467 -315)"
+                    >
+                      <path
+                        id="Path_40578"
+                        data-name="Path 40578"
+                        d="M528.754,633s.7,5.143,3.749,8.017,8.434,3.481,8.434,3.481h36.281s5.322.063,8.568-2.812S590.2,633,590.2,633a16.009,16.009,0,0,1,0,16.586c-3.748,7.363-14.325,7.1-14.325,7.1H540.936s-9.238-1.607-12.183-7.9C523.8,640.616,528.754,633,528.754,633Z"
+                        transform="translate(1244 189)"
+                        fill="#ffcb0b"
+                      />
+                      <path
+                        id="Path_40579"
+                        data-name="Path 40579"
+                        d="M576.447,664.044l-30.981.484s-7.509,1.959-11.726,6.478-5.623,11.915-5.623,11.915v30.256l11.647-12.317V684.8s.234-3.782,2.544-6.025S549,675.825,549,675.825l17-.8Z"
+                        transform="translate(1244.101 191.009)"
+                        fill="#ffcb0b"
+                      />
+                    </g>
+                  </g>
+                </svg>
+              </div>
+            </div>
+            <div className="row">
+              <div className="order-2 order-lg-1 col-lg-9 col-xl-8">
+                <div className="content">
+                  <div className="inner mb_sm--80">
+                    <h1 className="title">
+                      The Hyper Deflationary & Auto- Compounding Protocol in
+                      <span>Crypto</span>
+                    </h1>
+                    <div>
+                      <ul>
+                        <li className="description color_white">
+                          <p className="color_white">
+                            Highest Fixed APY - 211465.20%
+                          </p>
+                        </li>
+                        <li className="description color_white">
+                          <p className="color_white">
+                            First Automatic Staking and Compounding in Your
+                            Wallet!
+                          </p>
+                        </li>
+                        <li className="description color_white">
+                          <p className="color_white">
+                            Get Rewards Every 30 Minutes / 48 Times Daily!
+                          </p>
+                        </li>
+                        <li className="description color_white">
+                          <p className="color_white">
+                            Elastic Supply with no Minting Function
+                          </p>
+                        </li>
+                        <li className="description color_white">
+                          <p className="color_white">
+                            Auto burn Mechanism on every transaction
+                          </p>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+
+                  <div className="row ">
+                    <div className="col-lg-12 col-xl-6 col-md-12 col-sm-12 col-12">
+                      <div className="social-share-inner-left">
+                        <ul className="social-share d-flex liststyle">
+                          <li className="facebook">
+                            <a href="#">$Buy FINO</a>
+                          </li>
+                          <li className="instagram">
+                            <a href="#">White Paper</a>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+
+                  <p>Presale Starts on Feb 8th 2022, Tuesday 1.30 PM UTC</p>
+                  <h5 className="timerAlign" id="targetData"></h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
